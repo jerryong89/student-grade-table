0a0b9faa1d8079b94dd0677a5dc37cb0b77c6678
Jerry Vinh Ong <jerryong89@gmail.com>
2020-02-06 16:27:55 -0800

    added average grade, bootstrap cols, badge

--- components/page-header.js
+++ components/page-header.js
@@ -3,6 +3,7 @@ class PageHeader {
     this.headerElement = headerElement;
   }
   updateAverage(newAverage){
-    console.log(newAverage)
+    var averageNum = document.querySelector("span");
+    averageNum.append(newAverage);
   }
 }
