48b5b8b4f73befc679e4a6acbe34041e8140022c
Jerry Vinh Ong <jerryong89@gmail.com>
2020-02-07 10:58:31 -0800

    added grade form and first part of new grade

--- components/grade-form.js
+++ components/grade-form.js
@@ -2,12 +2,17 @@ class GradeForm {
   constructor(formElement){
     this.formElement = formElement;
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.formElement.addEventListener("submit", this.handleSubmit);
   }
   onSubmit(createGrade){
     this.onSubmit = onSubmit;
   }
   handleSubmit(event){
-    preventDefault(event);
+    event.preventDefault();
     console.log("hi");
+    var formData = new FormData(event.target);
+    formData.get(name, course, grade);
+    this.createGrade(name, course, grade);
+    event.target.reset()
   }
 }
--- components/main.js
+++ components/main.js
@@ -1,10 +1,8 @@
-var tbody = document.querySelector("tbody");
-var gradeTable = new GradeTable(tbody);
 var header = document.querySelector("header");
 var pageHeader = new PageHeader(header);
-
-// var  = document.querySelector();
-var gradeForm = new GradeForm(tbody);
-
+var tbody = document.querySelector("tbody");
+var gradeTable = new GradeTable(tbody);
+var form = document.querySelector("form");
+var gradeForm = new GradeForm(form);
 var app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
