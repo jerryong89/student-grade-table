473a5ed7c4a8c44d0f407d09d4854a1396b32192
Jerry Vinh Ong <jerryong89@gmail.com>
2020-02-06 17:58:00 -0800

    added gradeform sheet and class gradeform

---
+++ components/grade-form.js
@@ -0,0 +1,12 @@
+class GradeForm {
+  constructor(formElement){
+    this.formElement = formElement;
+  }
+  onSubmit(createGrade){
+    this.onSubmit = onSubmit;
+  }
+  handleSubmit(event){
+    preventDefault(event);
+    console.log("hi");
+  }
+}
